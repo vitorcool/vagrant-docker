7a88997da479b08afdd67ae6e3da1f15c3a37863
Vitor Oliveira <vitor.oliveira@galp.com>
2021-05-11 13:28:30 +0100

    fix aws.sh packer.sh

--- provision/scripts/aws.sh
+++ provision/scripts/aws.sh
@@ -7,7 +7,7 @@ save_bash_environment
 
 node_info
 # install aws
-apt-get  unzip  -y
+apt-get update && apt-get install unzip  -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 chmod +x ./aws/install
--- provision/scripts/packer.sh
+++ provision/scripts/packer.sh
@@ -1,15 +1,6 @@
-#
-# dockerEasy2
-#
-dd=/home/vagrant
-. $dd/provision/scripts/_context.sh
-save_bash_environment
-
-node_info
-
 
 # install packer
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt-get install packer -y
+apt-get update && apt-get install packer -y
 
