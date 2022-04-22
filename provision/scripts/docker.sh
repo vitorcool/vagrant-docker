d8783bf572f6a2d8c1cd0732419f4193590aa049
Vitor Oliveira <vitor.oliveira@galp.com>
2022-04-22 17:30:21 +0100

    update

--- provision/scripts/docker.sh
+++ provision/scripts/docker.sh
@@ -41,3 +41,16 @@ rm $DESTINATION
 curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
 chmod +x $DESTINATION
 chmod 755 $DESTINATION
+
+## krew
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+
+  echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> .bashrc
+)
